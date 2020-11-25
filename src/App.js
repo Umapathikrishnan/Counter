2ab8f57f08e6dde798825dc0c7b8206756507fc0
Umapathikrishnan <umapathikrishnank@gmail.com>
2020-11-25 21:07:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React,{useState} from "react";
+import "./style.css";
+
+function App() {
+  const [counter,setcounter]=useState(0);
+  function  count(){
+    setcounter(counter+1);
+  }
+  function  clear(){
+   setcounter(counter-1);
+  }
+  return (
+    <div>
+    <h1>Counter</h1>
+    <h1>{counter}</h1>
+<button onClick={count}>+</button>
+<button onClick={clear}>-</button>
+
+    </div>
+  );
+}
+export default App;
